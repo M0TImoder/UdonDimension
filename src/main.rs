386be8d23fe0f63c8b97a490da161755ea65ab32
M0TImoder <motimoder@gmail.com>
2025-10-30 21:08:01 +0900

    Rapierを導入

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,27 @@ use bevy::input::mouse::MouseMotion;
 use bevy::math::{primitives::{Cuboid, Plane3d}, EulerRot};
 use bevy::prelude::*;
 use bevy::window::{CursorGrabMode, PrimaryWindow};
+use bevy_rapier3d::prelude::*;
 
 fn main()
 {
     App::new()
         .add_plugins(
-            DefaultPlugins.set(
-                WindowPlugin
-                {
-                    primary_window: Some(
-                        Window
-                        {
-                            title: "UdonDimension Prototype".into(),
-                            ..default()
-                        }
-                    ),
-                    ..default()
-                }
+            (
+                DefaultPlugins.set(
+                    WindowPlugin
+                    {
+                        primary_window: Some(
+                            Window
+                            {
+                                title: "UdonDimension Prototype".into(),
+                                ..default()
+                            }
+                        ),
+                        ..default()
+                    }
+                ),
+                RapierPhysicsPlugin::<NoUserData>::default()
             )
         )
         .add_systems(Startup, setup)
@@ -41,23 +45,31 @@ fn setup(
     }
 
     commands.spawn(
-        PbrBundle
-        {
-            mesh: meshes.add(Mesh::from(Plane3d::default())),
-            material: materials.add(Color::rgb(0.5, 0.7, 0.5)),
-            transform: Transform::from_scale(Vec3::new(20.0, 1.0, 20.0)),
-            ..default()
-        }
+        (
+            PbrBundle
+            {
+                mesh: meshes.add(Mesh::from(Plane3d::default())),
+                material: materials.add(Color::rgb(0.5, 0.7, 0.5)),
+                transform: Transform::from_scale(Vec3::new(20.0, 1.0, 20.0)),
+                ..default()
+            },
+            RigidBody::Fixed,
+            Collider::cuboid(10.0, 0.5, 10.0)
+        )
     );
 
     commands.spawn(
-        PbrBundle
-        {
-            mesh: meshes.add(Mesh::from(Cuboid::from_size(Vec3::splat(1.0)))),
-            material: materials.add(Color::rgb(0.8, 0.2, 0.2)),
-            transform: Transform::from_xyz(0.0, 0.5, 0.0),
-            ..default()
-        }
+        (
+            PbrBundle
+            {
+                mesh: meshes.add(Mesh::from(Cuboid::from_size(Vec3::splat(1.0)))),
+                material: materials.add(Color::rgb(0.8, 0.2, 0.2)),
+                transform: Transform::from_xyz(0.0, 5.0, 0.0),
+                ..default()
+            },
+            RigidBody::Dynamic,
+            Collider::cuboid(0.5, 0.5, 0.5)
+        )
     );
 
     commands.spawn(
