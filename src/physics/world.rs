4caa94b4277aeab5619aac00e998604dd3b21911
M0TImoder <motimoder@gmail.com>
2025-11-16 14:41:21 +0900

    浮いていた箱を修正

--- src/physics/world.rs
+++ src/physics/world.rs
@@ -8,17 +8,28 @@ pub fn spawn_world(
     mut materials: ResMut<Assets<StandardMaterial>>
 )
 {
-    commands.spawn(
-        (
-            PbrBundle
-            {
-                mesh: meshes.add(Mesh::from(Plane3d::default())),
-                material: materials.add(Color::rgb(0.5, 0.7, 0.5)),
-                transform: Transform::from_scale(Vec3::new(20.0, 1.0, 20.0)),
-                ..default()
-            },
-            RigidBody::Fixed,
-            Collider::cuboid(10.0, 0.5, 10.0)
+    commands
+        .spawn(
+            (
+                PbrBundle
+                {
+                    mesh: meshes.add(Mesh::from(Plane3d::default())),
+                    material: materials.add(Color::rgb(0.5, 0.7, 0.5)),
+                    transform: Transform::from_scale(Vec3::new(20.0, 1.0, 20.0)),
+                    ..default()
+                },
+                RigidBody::Fixed
+            )
         )
-    );
+        .with_children(|parent|
+        {
+            parent.spawn(
+                (
+                    TransformBundle::from(
+                        Transform::from_translation(Vec3::new(0.0, -0.5, 0.0))
+                    ),
+                    Collider::cuboid(10.0, 0.5, 10.0)
+                )
+            );
+        });
 }
