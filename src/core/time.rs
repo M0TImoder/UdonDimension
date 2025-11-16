fa8cec9daa01457b02924a3146d349a8c05fedb9
M0TImoder <motimoder@gmail.com>
2025-11-16 15:30:13 +0900

    ティックの定義とイベントの窓口を作成

--- src/core/time.rs
+++ src/core/time.rs
@@ -3,22 +3,117 @@ use crate::core::units::Seconds;
 
 pub struct TimeManagerPlugin;
 
+pub const TICK_DURATION: Seconds = Seconds(0.01);
+
+#[allow(dead_code)]
+#[derive(Event)]
+pub struct TickEvent
+{
+    pub tick: u64,
+}
+
+#[allow(dead_code)]
+#[derive(Event)]
+pub struct TenTickEvent
+{
+    pub tick: u64,
+}
+
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub enum TimeSystem
+{
+    Accumulate,
+    Notify,
+}
+
 impl Plugin for TimeManagerPlugin
 {
     fn build(&self, app: &mut App)
     {
-        app.init_resource::<SimulationTime>()
-            .add_systems(Update, accumulate_time);
+        app.add_event::<TickEvent>()
+            .add_event::<TenTickEvent>()
+            .init_resource::<SimulationTime>()
+            .configure_sets(Update, (TimeSystem::Accumulate, TimeSystem::Notify).chain())
+            .add_systems(Update, accumulate_time.in_set(TimeSystem::Accumulate))
+            .add_systems(Update, dispatch_ten_tick_events.in_set(TimeSystem::Notify));
     }
 }
 
-#[derive(Resource, Default)]
+#[derive(Resource)]
 pub struct SimulationTime
 {
     pub elapsed: Seconds,
+    pub ticks: u64,
+    sub_tick: Seconds,
 }
 
-fn accumulate_time(time: Res<Time>, mut simulation_time: ResMut<SimulationTime>)
+impl Default for SimulationTime
 {
-    simulation_time.elapsed += Seconds::from(time.delta_seconds());
+    fn default() -> Self
+    {
+        Self
+        {
+            elapsed: Seconds::default(),
+            ticks: 0,
+            sub_tick: Seconds::default(),
+        }
+    }
+}
+
+fn accumulate_time(
+    time: Res<Time>,
+    mut simulation_time: ResMut<SimulationTime>,
+    mut tick_events: EventWriter<TickEvent>
+)
+{
+    let delta_seconds = Seconds::from(time.delta_seconds());
+
+    simulation_time.elapsed += delta_seconds;
+    simulation_time.sub_tick += delta_seconds;
+
+    let tick_length = TICK_DURATION.value();
+
+    if simulation_time.sub_tick.value() < tick_length
+    {
+        return;
+    }
+
+    let completed_ticks = (simulation_time.sub_tick.value() / tick_length).floor() as u64;
+
+    if completed_ticks == 0
+    {
+        return;
+    }
+
+    let start_tick = simulation_time.ticks + 1;
+
+    simulation_time.ticks += completed_ticks;
+    simulation_time.sub_tick -= Seconds::from(tick_length * completed_ticks as f32);
+
+    for tick in start_tick..=simulation_time.ticks
+    {
+        tick_events.send(TickEvent { tick });
+    }
+}
+
+fn dispatch_ten_tick_events(
+    simulation_time: Res<SimulationTime>,
+    mut last_group: Local<u64>,
+    mut event_writer: EventWriter<TenTickEvent>,
+)
+{
+    let current_group = simulation_time.ticks / 10;
+
+    if current_group <= *last_group
+    {
+        return;
+    }
+
+    for index in (*last_group + 1)..=current_group
+    {
+        let tick_value = index * 10;
+        event_writer.send(TenTickEvent { tick: tick_value });
+    }
+
+    *last_group = current_group;
 }
--- src/robot/mod.rs
+++ src/robot/mod.rs
@@ -3,6 +3,7 @@ pub mod drive;
 use bevy::math::primitives::Cuboid;
 use bevy::prelude::*;
 use bevy_rapier3d::prelude::*;
+use crate::core::time::{TickEvent, TimeSystem};
 use drive::DriveInput;
 
 pub struct RobotPlugin;
@@ -11,7 +12,8 @@ impl Plugin for RobotPlugin
 {
     fn build(&self, app: &mut App)
     {
-        app.add_systems(Startup, spawn_robot);
+        app.add_systems(Startup, spawn_robot)
+            .add_systems(Update, apply_drive_input_velocity.after(TimeSystem::Accumulate));
     }
 }
 
@@ -32,7 +34,45 @@ fn spawn_robot(
             },
             RigidBody::Dynamic,
             Collider::cuboid(0.5, 0.5, 0.5),
+            Velocity::default(),
             DriveInput::default()
         )
     );
+}
+
+fn apply_drive_input_velocity(
+    mut tick_events: EventReader<TickEvent>,
+    mut query: Query<(&DriveInput, &Transform, &mut Velocity), With<RigidBody>>
+)
+{
+    for _event in tick_events.read()
+    {
+        for (drive_input, transform, mut velocity) in query.iter_mut()
+        {
+            let mut forward = Vec3::from(transform.forward());
+            forward.y = 0.0;
+
+            if let Some(normalized_forward) = forward.try_normalize()
+            {
+                forward = normalized_forward;
+            }
+
+            let mut right = Vec3::from(transform.right());
+            right.y = 0.0;
+
+            if let Some(normalized_right) = right.try_normalize()
+            {
+                right = normalized_right;
+            }
+
+            let mut linear_velocity = velocity.linvel;
+            linear_velocity.x = (forward * drive_input.vx.value()).x
+                + (right * drive_input.vy.value()).x;
+            linear_velocity.z = (forward * drive_input.vx.value()).z
+                + (right * drive_input.vy.value()).z;
+
+            velocity.linvel = linear_velocity;
+            velocity.angvel = Vec3::new(0.0, drive_input.omega.value(), 0.0);
+        }
+    }
 }
